5ff80a1e46c16b5aa4e0672187808c886465469d
agnescameron <agnesfcameron@gmail.com>
2020-07-22 12:15:54 -0400

    corrections

--- wikidata.js
+++ wikidata.js
@@ -7,6 +7,15 @@ function isObject(obj)
     return obj != null && obj.constructor.name === "Object"
 }
 
+const remoteRepo = 'https://github.com/agnescameron/wikidata'
+
+const context = {
+	"@vocab": "http://purl.org/dc/terms/",
+	"prov": "http://www.w3.org/ns/prov#",
+}
+
+const fetchURL = `https://www.wikidata.org/w/api.php?action=wbgetentities&ids=${entity}&format=json&props=sitelinks|labels|descriptions|claims`
+
 try{
 	fetch(`https://www.wikidata.org/w/api.php?action=wbgetentities&ids=${entity}&format=json&props=sitelinks|labels|descriptions|claims`)
 		.then(response => response.json())
@@ -14,12 +23,37 @@ try{
 		
 		//iterate claims as assertions
 		for(claim in data.entities[entity].claims){
+			var now = new Date();
+
+			const claimType = data.entities[entity].claims[claim][0].mainsnak.datavalue.type;
 			let claimVal = data.entities[entity].claims[claim][0].mainsnak.datavalue.value;
 			claimVal = isObject(claimVal) ? claimVal.id : claimVal;
-			const assertion = `<${entity}><${claim}><${claimVal}>`;
+
+			const claimGraph = {
+				"@context": context,
+				"prov:wasDerivedFrom": {
+					"@id": fetchURL
+				},
+				"prov:wasGeneratedBy": {
+					"prov:startedAtTime": {
+						"@value": now.toISOString(),
+						"@type": "http://www.w3.org/2001/XMLSchema#dateTime"
+					},
+					"prov:used": {
+						"@id": remoteRepo
+					}
+				},
+				"@graph": {
+					"@type": "Claim",
+					"subject": entity,
+					"property": claim,
+					"qualifier": claimType,
+					"qualifierValue": claimVal
+				}
+			};
 
 			//write assertion to file
-			fs.writeFile(`package/${claim}.nt`, assertion, function (err) {
+			fs.writeFile(`package/${claim}.jsonld`, JSON.stringify(claimGraph, null, 4), function (err) {
 			  if (err) return console.log(err);
 			});
 		};
